5490e7290063b4029a985af783ba816cf803a1bb
Evgeny Ganusenko <evgan66@gmail.comevgan66@gmail.comevgan66@gmail.com>
2022-02-03 22:59:53 +0300

    Initial Commit

---
+++ AppleMusicApp/AppleMusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicAppApp.swift
+//  AppleMusicApp
+//
+//  Created by Евгений Ганусенко on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
